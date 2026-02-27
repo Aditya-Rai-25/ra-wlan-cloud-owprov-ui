f19bc70c2b38dfbb2a4859559e22c20c4c99bf77
Aditya-Rai-25 <aditya21.rai@routerarchitects.com>
2026-02-27 15:19:59 +0530

    [PROV][UI] Update Mango Cloud logo asset and login styling (#28)
    - Copied new Mango Cloud logo as `manglo-cloud.png`
     - Routed layout/login to use it.

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import darkLogo from 'assets/Logo_Dark_Mode.svg';
-import lightLogo from 'assets/Logo_Light_Mode.svg';
+import darkLogo from 'assets/mango-cloud.png';
+import lightLogo from 'assets/mango-cloud.png';
 import { useAuth } from 'contexts/AuthProvider';
 
 const Layout = React.lazy(() => import('layout'));
